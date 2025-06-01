e5d7da0b4cd497e7a3a1da33d6157314fdbe1856
Miguel Mosquera <mopri38@gmail.com>
2025-05-31 21:39:52 -0500

    refactor(mapper): remove unused imports and INSTANCE references in Favority and User mappers

--- src/main/java/co/uco/golocal/golocalapi/data/mapper/concrete/IUserMapperEntity.java
+++ src/main/java/co/uco/golocal/golocalapi/data/mapper/concrete/IUserMapperEntity.java
@@ -4,27 +4,9 @@ import co.uco.golocal.golocalapi.data.entity.user.UserEntity;
 import co.uco.golocal.golocalapi.domain.user.UserDomain;
 import org.mapstruct.Mapper;
 
-import org.mapstruct.factory.Mappers;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Mapper(componentModel = "spring")
 public interface IUserMapperEntity {
 
-	IUserMapperEntity INSTANCE= Mappers.getMapper(IUserMapperEntity.class);
-
 	UserEntity toEntity(UserDomain usuarioDomain);
 	UserDomain toDomain(UserEntity entidad);
-
-	default Page<UserDomain> UsuarioEntidadPageToUsuarioPage(Page<UserEntity> usuarioEntidadPage) {
-		List<UserDomain> usuarioDomainList = usuarioEntidadPage.getContent()
-				.stream()
-				.map(this::toDomain)
-				.collect(Collectors.toList());
-		return new PageImpl<>(usuarioDomainList, usuarioEntidadPage.getPageable(), usuarioEntidadPage.getTotalElements());
-	}
-
 }
